3b0eede4be48b15345254d460c2c1269de589e44
bogdashk <bogdud8@gmail.com>
2023-08-02 17:17:40 +0300

    Fifth commit

--- advertisement/advertisement/urls.py
+++ advertisement/advertisement/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/', include('app_advertisement.urls')),
+    path('', include('app_advertisement.urls')),
     path('lesson_4', include('app_lesson_4.urls'))
 ]
--- advertisement/app_advertisement/views.py
+++ advertisement/app_advertisement/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def fun(requests):
-    return HttpResponse('yeah good!!!!!!!!!')
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
